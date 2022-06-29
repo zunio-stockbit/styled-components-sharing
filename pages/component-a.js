cdc2683e91589d7f27b443be89ef32b1a12c2758
Zunio <zunio.benarivo@stockbit.com>
2022-06-29 20:53:28 +0700

    feat: test render styled components

---
+++ pages/component-a.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+export default function ComponentA() {
+  return (
+    <div>
+      <Title>HEHE</Title>
+    </div>
+  )
+}
